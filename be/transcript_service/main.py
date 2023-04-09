eb0ea55c4286245a3081f0d6bb2647c912b69c76
Vandivier <john@afterecon.com>
2023-04-09 16:55:35 -0400

    quizzes

--- be/transcript_service/main.py
+++ be/transcript_service/main.py
@@ -1,12 +1,9 @@
 from typing import Dict, List, Union
 from youtube_transcript_api import YouTubeTranscriptApi
 
-
-def main() -> None:
-    TimeRangeWithText = Dict[str, Union[float, str]]
-    video_ids = ["6C9JsyfDBe8", "pmgYUxba0cc", "t4Kndozwnms", "1Vv0PkSsA0k", "-1vqVgtjDOM"]
-
-    with open("prompt.txt", "w") as f:
+def get_flashcards_file_substr() -> str:
+    file_substr = "flashcards"
+    with open(f"prompt-for-{file_substr}.txt", "w") as f:
         f.write("""
         consider the following web video transcripts, dileneated by their sourcePlatformId.
         the sourcePlatformName for all of these is "YouTube Shorts".
@@ -23,7 +20,49 @@ def main() -> None:
                 sourcePlatformName String
             }
         ```
-                """)
+        """)
+    return file_substr
+
+def get_quizzes_file_substr() -> str:
+    file_substr = "quizzes"
+    with open(f"prompt-for-{file_substr}.txt", "w") as f:
+        f.write("""
+        consider the following web video transcripts, dileneated by their sourcePlatformId.
+        the sourcePlatformName for all of these is "YouTube Shorts".
+        please generate at least one QuizItem per transcript,
+        and ten in total.
+        these ten QuizItems should be organized into a single Quiz.
+        a flash card has a question on the front and the answer on the back.
+        organize the flash cards as JSON data following the format below:
+        ```Prisma
+            model PossibleAnswer {
+                id   Int
+                text String
+            }
+
+            model QuizItem {
+                id           Int
+                questionText        String
+                correctAnswerId     String
+                possibleAnswerIds   Int[]
+            }
+
+            model Quiz {
+                id                 Int
+                quizItemIds        Int[]
+                sourcePlatformId   String
+                sourcePlatformName String
+            }
+        ```
+        """)
+    return file_substr
+
+def main() -> None:
+    TimeRangeWithText = Dict[str, Union[float, str]]
+    video_ids = ["6C9JsyfDBe8", "pmgYUxba0cc", "t4Kndozwnms", "1Vv0PkSsA0k", "-1vqVgtjDOM"]
+
+    flashcards_file_substr = get_flashcards_file_substr()
+    quizzes_file_substr = get_quizzes_file_substr()
 
     for video_id in video_ids:
         transcript: List[TimeRangeWithText] = YouTubeTranscriptApi.get_transcript(
@@ -35,12 +74,17 @@ def main() -> None:
         transcript_text = " ".join([item["text"] for item in transcript])
         prompt_text  = transcript_text
 
-        with open("prompt.txt", "a") as f:
+        with open(f"prompt-for-{flashcards_file_substr}.txt", "a") as f:
             f.write(f"\nurl: https://youtube.com/shorts/{video_id}")
             f.write(f"\ntext: {prompt_text}")
+            # TODO: send prompt.txt to LLM, validate result is JSON,
+            # and save to flashcards.json
 
-    # TODO: send prompt.txt to LLM, validate result is JSON,
-    # and save to flashcards.json
+        with open(f"prompt-for-{quizzes_file_substr}.txt", "a") as f:
+            f.write(f"\nurl: https://youtube.com/shorts/{video_id}")
+            f.write(f"\ntext: {prompt_text}")
+            # TODO: send prompt.txt to LLM, validate result is JSON,
+            # and save to flashcards.json
 
 
 if __name__ == "__main__":
