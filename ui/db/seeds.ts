b8a359007c23a7dc72cfec22d638587cdd08b032
Vandivier <john@afterecon.com>
2023-04-16 15:13:52 -0400

    feat: working seed

--- ui/db/seeds.ts
+++ ui/db/seeds.ts
@@ -1,15 +1,91 @@
-// import db from "./index"
+// ref: https://www.prisma.io/docs/guides/migrate/seed-database
+import db, { Prisma, Quiz, QuizItem, PossibleAnswer } from "./index"
+import quizzes from "../../be/transcript_service/outputs/quizzes.json"
 
-/*
- * This seed function is executed when you run `blitz db seed`.
- *
- * Probably you want to use a library like https://chancejs.com
- * to easily generate realistic data.
- */
 const seed = async () => {
-  // for (let i = 0; i < 5; i++) {
-  //   await db.project.create({ data: { name: "Project " + i } })
-  // }
+  // Seed quizzes
+  await Promise.all(
+    quizzes.map(async (quizData) => {
+      const { quiz: quizInputData, quizItems, possibleAnswers } = quizData
+
+      const quiz: Quiz = await db.quiz.create({
+        data: {
+          inputId: quizInputData.inputId,
+          name: quizInputData.name,
+          sourcePlatformId: quizInputData.sourcePlatformId,
+          sourcePlatformName: quizInputData.sourcePlatformName,
+        },
+      })
+
+      // Seed quiz items
+      const quizItemMap = new Map<string, QuizItem>()
+      await Promise.all(
+        quizItems.map(async (quizItemData) => {
+          const { inputId, questionText, possibleAnswerInputIds } = quizItemData
+
+          const quizItemCreateInput: Prisma.QuizItemCreateInput = {
+            inputId,
+            questionText,
+            quiz: { connect: { id: quiz.id } },
+          }
+          const quizItem = await db.quizItem.create({
+            data: quizItemCreateInput,
+          })
+
+          quizItemMap.set(quizItemData.inputId, quizItem)
+
+          // Seed possible answers
+          await Promise.all(
+            possibleAnswerInputIds.map(async (possibleAnswerId) => {
+              const possibleAnswerData = possibleAnswers.find(
+                (pa) => pa.inputId === possibleAnswerId
+              )
+
+              if (possibleAnswerData) {
+                await db.possibleAnswer.create({
+                  data: {
+                    inputId: possibleAnswerData.inputId,
+                    text: possibleAnswerData.text,
+                    quizItem: { connect: { id: quizItem.id } },
+                  },
+                })
+              }
+            })
+          )
+        })
+      )
+
+      // Connect quiz items with correct answers and possible answers
+      await Promise.all(
+        quizItems.map(async (quizItemData) => {
+          const quizItem = quizItemMap.get(quizItemData.inputId)
+
+          if (quizItem) {
+            const correctAnswer = await db.possibleAnswer.findUniqueOrThrow({
+              where: { inputId: quizItemData.correctAnswerId },
+            })
+            const possibleAnswerIds = quizItemData.possibleAnswerInputIds
+
+            if (correctAnswer && possibleAnswerIds.includes(correctAnswer.inputId)) {
+              const possibleAnswersToConnect: Prisma.Enumerable<Prisma.PossibleAnswerWhereUniqueInput> =
+                possibleAnswerIds.map((inputId) => ({ inputId }))
+
+              await db.quizItem.update({
+                where: { id: quizItem.id },
+                data: {
+                  correctAnswer: { connect: { id: correctAnswer.id } },
+                  possibleAnswers: { connect: possibleAnswersToConnect },
+                },
+              })
+            } else {
+              console.log({ quizItem, correctAnswer, possibleAnswerIds })
+              throw new Error(`Super mean quiz item!! with inputId: ${quizItem.inputId}`)
+            }
+          }
+        })
+      )
+    })
+  )
 }
 
 export default seed
